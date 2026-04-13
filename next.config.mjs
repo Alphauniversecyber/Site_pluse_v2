0be3d684f4acd23169a444bacfd806efe3fd9754
Alphauniversecyber <alphauniversecourses@gmail.com>
2026-04-13 13:43:18 +0530

    up

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: [
-    "@sparticuz/chromium",
-    "puppeteer",
-    "puppeteer-core",
-    "linkinator",
-    "metascraper",
-    "metascraper-description",
-    "metascraper-image",
-    "metascraper-title",
-    "metascraper-url",
-    "re2",
-    "url-regex-safe"
-  ],
-  outputFileTracingIncludes: {
-    "/api/cron/process-reports": ["node_modules/@sparticuz/chromium/**/*"],
-    "/api/reports/generate": ["node_modules/@sparticuz/chromium/**/*"]
-  },
   experimental: {
-    typedRoutes: true
+    typedRoutes: true,
+    serverComponentsExternalPackages: [
+      "@sparticuz/chromium",
+      "puppeteer",
+      "puppeteer-core",
+      "linkinator",
+      "metascraper",
+      "metascraper-description",
+      "metascraper-image",
+      "metascraper-title",
+      "metascraper-url",
+      "re2",
+      "url-regex-safe"
+    ],
+    outputFileTracingIncludes: {
+      "/api/cron/process-reports": ["node_modules/@sparticuz/chromium/**/*"],
+      "/api/reports/generate": ["node_modules/@sparticuz/chromium/**/*"]
+    }
   },
   images: {
     remotePatterns: [
