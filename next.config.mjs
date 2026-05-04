cc66ab40d814056c0778d1d90d2dbf6c72513bc4
Alphauniversecyber <alphauniversecourses@gmail.com>
2026-05-04 09:31:54 +0530

    uodate

--- next.config.mjs
+++ next.config.mjs
@@ -44,22 +44,6 @@ const nextConfig = {
       }
     ]
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.trysitepulse.com" }],
-        destination: "https://trysitepulse.com/:path*",
-        permanent: true
-      },
-      {
-        source: "/:path*",
-        has: [{ type: "header", key: "x-forwarded-proto", value: "http" }],
-        destination: "https://trysitepulse.com/:path*",
-        permanent: true
-      }
-    ];
-  },
   async headers() {
     return [
       {
